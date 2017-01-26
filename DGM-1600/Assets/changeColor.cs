5111c7e08e22a6fc60a4eeaea890efb7c17519bd
Unknown <kyledpinkham@Gmail.com>
2017-01-26 11:13:50 -0700

    Go.
    Go to the repo darn you

---
+++ DGM-1600/Assets/changeColor.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using System.Collections;
+
+	public class changeColor : MonoBehaviour {
+
+		void Update () {
+				if(Input.GetKeyDown(KeyCode.R)){
+					GetComponent<Renderer>().material.color = Color.red;
+				}
+				if(Input.GetKeyDown(KeyCode.G)){
+					GetComponent<Renderer>().material.color = Color.green;
+				}
+				if(Input.GetKeyDown(KeyCode.B)){
+					GetComponent<Renderer>().material.color = Color.blue;
+				}
+				if (Input.GetKeyDown(KeyCode.Space)) {
+					GetComponent<Renderer>().material.color = Color.black;
+				}
+				if (Input.GetKeyDown(KeyCode.Delete)) {
+					GetComponent<Renderer>().material.color = Color.white;
+				}
+				if (Input.GetKeyDown(KeyCode.Escape)) {
+					GetComponent<Renderer>().material.color = Color.yellow;
+				}
+		}
+	}
