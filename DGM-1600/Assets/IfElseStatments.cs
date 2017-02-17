9eb689684787f06faac313e561aa111b4452000c
Unknown <kyledpinkham@Gmail.com>
2017-02-16 23:39:36 -0700

    adding new assets

--- DGM-1600/Assets/IfElseStatments.cs
+++ DGM-1600/Assets/IfElseStatments.cs
@@ -4,14 +4,16 @@ using UnityEngine;
 
 public class IfElseStatments : MonoBehaviour
 {
-    private float grade;
+    private float grade = 99f;
     bool death = true;
+    string flower = "Tulips";
+    float speed = 0f;
+    float distance = 100.0f;
+    float time = 20.0f;
 
     // Use this for initialization
     void Start()
     {
-
-
         //This line of code creates a loop- as long as i <= 100, it will add i+1, with I starting at 1
         for (int i = 1; i <= 100; i++)
         {
@@ -28,20 +30,54 @@ public class IfElseStatments : MonoBehaviour
                 print("Fizz");
             else if (buzz)
                 print("Buzz");
-            // This one is super important, and shows why you should use an Else loop- Forgetting it will cause the console 
+            // This one is super important, and shows why you should use an Else in a loop- Forgetting it will cause the console 
             // to print out just a bunch of Fizzes and Buzzes with no apparent meaning. 
             else
                 print(i);
         }
-            if(death)
-        {
+
+        if (death)
+            {
             print("I like beef jerky");
-            else if (!death)
-                print("I still like beef jerky even though I'm aliveee");
-            else
-                print("I am going to make a game for my son.");
-        }
             }
-            }
+        else if (!death)
+            print("I still like beef jerky even though I'm aliveee");
+        else
+            print("I am going to make a game for my son.");
+
+        // This code made me learn the importance of making sure everything was in the right code block.
+        if (flower == "Tulips")
+            print("Tulips are a sweet smelling flower:");
+        else
+            print("I don't care about other flowers much");
+
+        // this code checks for gradez and makes you feel bad about yourself
+        if (grade == 100)
+            print("A perfect Score?!.. Thats... Surprising.");
+        else if (grade >= 80)
+            print("Thats a B. Go grab a beer or something");
+        else if (grade >= 60)
+            print("You couldn't even get a B... thats depressing");
+        else
+            print("yeah... I figured you would get that grade. Slime.");
+
+        speed = (distance / time);
+
+        // there might be a better way to do this, but this should allow it to return a value for every number while also
+        //returning a value if the number comes back as exactly zero. Full disclosure- I'm trying to be creative but I also need to turn in the assignment.
+        if (speed >= 10)
+            print("Slow the FRICK down");
+        else if (speed <= 10 || speed != 0)
+            print("You arent going fast enough");
+        else
+            print("You arent moving at all");
+
+
+
+
+    }
+}
+
+         
 
  
---
+++ DGM-1600/Assets/switchStatment.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class switchStatment : MonoBehaviour {
+
+    private int time = 10;
+
+    public string animal = "Alligator";
+    private string phrase;
+    private bool exists = true;
+    
+    // Use this for initialization
+	void start () {
+
+        switch (animal)
+        {
+            case "Giraffe":
+                phrase = "Wheezy neighbaaaah";
+                break;
+            case "Penguin":
+                phrase = "Morgan Freeman";
+                break;
+            case "Fox":
+                phrase = "whapapapapapapow";
+                break;
+            case "Alligator":
+                phrase = "Would you like some tea with that, sir?";
+                break;
+            default:
+                exists = false;
+                break;
+        }
+
+        if (exists)
+            print("A " + animal + " says \"" + phrase + "\".");
+        else
+            print(animal + " does not exist.");
+        
+        switch(time)
+        {
+            case 9:
+                print("good morning!");
+                break;
+            case 12:
+                print("Lunch Time!");
+                break;
+            case 18:
+                print("Get the heck out of the office right now you slime");
+                break;
+            default:
+                print("get back to work, slime!!~");
+                break;
+        }
+		
+	}
+
+}
