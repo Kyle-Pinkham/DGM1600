dde72c3657e4221aa32bd9c6ed91e47f8fe25fde
Unknown <kyledpinkham@Gmail.com>
2017-04-26 10:25:10 -0600

    Deleted unneccissary prefabs and also cleaned up some of my code. Added a score tracker.

--- CalcGame/CalcGame/Assets/Scripts/CalcSkript.cs
+++ CalcGame/CalcGame/Assets/Scripts/CalcSkript.cs
@@ -10,21 +10,20 @@ public class CalcSkript : MonoBehaviour {
     public Button subButton;
     public Button divButton;
     public Button mulButton;
-
     public float Num1;
     public float Num2;
     public float Result = 0;
     public InputField InNum1;
     public InputField InNum2;
     public Text resultNum;
+    public Text score;
+    public static int scoretracker = 0;
     private bool buttonActiveChecker = true;
-
-
-    void Start() {
-    }
+    //Class variables Declared. Begin functions:
 
     void Update() {
         resultNum.text = "" + Result.ToString();
+        score.text = "Score:" + scoretracker.ToString();
     }
 
     public static bool NumberChecker(float x) {
@@ -38,16 +37,15 @@ public class CalcSkript : MonoBehaviour {
                 audioFile.Play();
                         if (x == float.Parse(circleMesh.text)) {
                             Destroy(circle.transform.parent.gameObject);
+                            scoretracker++;
                             result = true;
                         }
                 }
-
         }
         return result;   
     }
 
     public void ButtonInactive(Button butt) {
-
         if (NumberChecker(Result)) {
             butt.interactable = false;
             if (addButton.interactable == false &&
@@ -61,28 +59,23 @@ public class CalcSkript : MonoBehaviour {
         }
     }
 
+    //Comment line for Ease of access purposes. This seperates the button action functions from the mechanical functions. 
+
     public void ButtonAdd() {
         Result = (float.Parse(InNum1.text) + float.Parse(InNum2.text));
         ButtonInactive(addButton);
     }
-
     public void ButtonSub() {
         Result = (float.Parse(InNum1.text) - float.Parse(InNum2.text));
         ButtonInactive(subButton);
-
     }
-
     public void ButtonDiv() {
         Result = (float.Parse(InNum1.text) / float.Parse(InNum2.text));
         ButtonInactive(divButton);
-
     }
-
     public void ButtonMul() {
         Result = (float.Parse(InNum1.text) * float.Parse(InNum2.text));
         ButtonInactive(mulButton);
-
     }
-
-
+//End Bracket
 }
